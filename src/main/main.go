f556ce5b3c66d59d0b3a71f52005f605f286358e
Firemark <marpiechula@gmail.com>
2016-01-16 17:11:15 +0100

    add randomed brick

--- src/main/main.go
+++ src/main/main.go
@@ -5,25 +5,36 @@ import (
     "os"
     "log"
     "pixelopolis"
+    "math/rand"
+    //"math"
     //"fmt"
 )
 
-func brick(a pixelopolis.PixelAttrs) pixelopolis.RoyalPixel {
-    cond := a.Y % 4 != 0 && a.X % 8 != 0
-    var brick_color [3]byte
-    if (cond) {
-        brick_color = [3]byte{80, 25, 33}
-    } else {
-        brick_color = [3]byte{215, 215, 215}
+func brick() func(a pixelopolis.PixelAttrs) pixelopolis.RoyalPixel {
+    move_y := rand.Int()
+    move_x := rand.Int()
+    return func(a pixelopolis.PixelAttrs) pixelopolis.RoyalPixel {
+        y := a.Y + move_y
+        x := a.X + move_x
+        cond := y % 4 != 0 && (x + (y / 4) * 3) % 8 != 0
+        var brick_color [3]byte
+        if (cond) {
+            brick_color = [3]byte{80, 25, 33}
+        } else {
+            brick_color = [3]byte{215, 215, 215}
+        }
+        return pixelopolis.FromRGB(brick_color)  
     }
-    return pixelopolis.FromRGB(brick_color)
 }
 
 func main() {
     rimage := pixelopolis.NewRoyalImage(512, 512)
-    drw := pixelopolis.Drawer{Img: &rimage, Fn: brick}
+    drw := pixelopolis.Drawer{Img: &rimage, Fn: brick()}
     drw.CreateCylinder([2]int{256, 256}, [2]int{64, 48})
     drw.CreateCone([2]int{256, 256 - 48}, 64)
+    drw.CreateCuboid([2]int{128, 128}, [3]int{32,32,64})
+    drw.CreateCuboid([2]int{128, 220}, [3]int{32,32,64})
+    drw.CreateCuboid([2]int{128, 300}, [3]int{32,32,64})
     file, err := os.Create("test.png")
     if err != nil {
         log.Fatal(err)
